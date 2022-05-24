3d2f3730afc4a94664d9154077eca8230b547198
pakyinw <pakyinwww@gmail.com>
2022-05-25 07:39:43 +0800

    build(core): Update outline

---
+++ src/Bubble.js
@@ -0,0 +1,9 @@
+import './Bubble.css';
+
+const Bubble = () => {
+    return (
+        <div className="Bubble">Bubble</div>
+    )
+}
+
+export default Bubble;
---
+++ src/Character.js
@@ -0,0 +1,9 @@
+import './Character.css'
+
+const Character = () => {
+    return (
+        <div className="Character">Character</div>
+    )
+}
+
+export default Character;
